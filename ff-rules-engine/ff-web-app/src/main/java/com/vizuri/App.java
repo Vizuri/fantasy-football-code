36856ebd4f4108fdc7fb3297c97c9869f2dde2d2
Anil Mirge <amirge@vizuri.com>
2014-03-26 12:05:45 -0400

    First commit with a maven based multi module project shell

---
+++ ff-rules-engine/ff-web-app/src/main/java/com/vizuri/App.java
@@ -0,0 +1,13 @@
+package com.vizuri;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
